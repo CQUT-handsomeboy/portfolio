8a59ffcdba7ffc353c1d91621b2c21d9b72f7d95
ColdRiver <17830831606@163.com>
2026-01-12 16:10:06 +0800

    delete blog

--- components/header.tsx
+++ components/header.tsx
@@ -15,12 +15,12 @@ export function Header() {
                     </div>
 
                     <nav className="flex items-center space-x-6">
-                        <a
+                        {/* <a
                             href="/blog"
                             className="text-gray-600 hover:text-blue-600 transition-colors duration-200 font-medium"
                         >
                             <BookmarkMinus className='w-6 h-6' />
-                        </a>
+                        </a> */}
 
                         {/* GitHub Link */}
                         <a
